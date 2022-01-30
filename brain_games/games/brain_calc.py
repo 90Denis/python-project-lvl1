13cfdc9465b2ae57fe3ac20dd5529ee0d13fd298
90Denis <d.shishkin90@gmail.com>
2022-01-30 20:26:53 +0300

    Add step 6

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,20 @@
+from random import randint, choice
+from operator import add, sub, mul
+
+GAME_RULE = 'What is the result of the expression?'
+OPERATIONS = (
+    ('+', add),
+    ('-', sub),
+    ('*', mul),
+)
+
+
+def get_task_and_solution():
+    number1 = randint(0, 10)
+    number2 = randint(0, 10)
+    random_operation = choice(OPERATIONS)
+
+    correct_answer = random_operation[1](number1, number2)
+    task = '{1} {0} {2}'.format(random_operation[0], number1, number2)
+
+    return task, str(correct_answer)
